991357b690453e36ef68d18001e17ae28291fd49
Vlad-Bailo-Politech <ipzk231_bvv@student.ztu.edu.ua>
2025-06-26 12:26:05 +0300

    New user route file created.

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/auth"); //
+const userRoutes = require("./routes/user"); //
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes); //
-
+app.use("/api/users", userRoutes); //
 
 const PORT = process.env.PORT || 5000;
 
